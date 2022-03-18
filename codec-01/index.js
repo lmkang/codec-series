8f055e2f46804f75d8a4d73a2121b92a369b56b3
0027024154 <liang.mingkang@iwhalecloud.com>
2022-03-18 17:32:55 +0800

    codec-01

---
+++ codec-01/index.js
@@ -0,0 +1,52 @@
+var video = document.getElementById('player');
+var hls = new Hls();
+hls.loadSource('http://localhost:9494/m3u8/index.m3u8');
+hls.attachMedia(video);
+hls.on(Hls.Events.MEDIA_ATTACHED, function() {
+    video.play();
+});
+
+httpGet('http://localhost:9494/m3u8/index.m3u8', 'text', function(content) {
+    console.log(parseM3U8('http://localhost:9494/m3u8/', content));
+});
+
+function parseM3U8(prefix, content) {
+    if(prefix == null) {
+        prefix = '';
+    }
+    var lines = content.split(/\r?\n|\r\n?/);
+    var list = [];
+    for(var i = 0; i < lines.length; i++) {
+        if(lines[i].indexOf('#EXT-X-ENDLIST') === 0) {
+            break;
+        }
+        if(lines[i].indexOf('#EXTINF') === 0) {
+            var start = lines[i].indexOf(':');
+            var end = lines[i].indexOf(',');
+            var value = lines[i].substring(start + 1, end);
+            list.push({
+                duration: parseFloat(value),
+                url: prefix + lines[++i]
+            });
+        }
+    }
+    return list;
+}
+
+function httpGet(url, responseType, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('get', url, true);
+    xhr.responseType = responseType;
+    xhr.onload = function () {
+        if(xhr.status === 200) {
+            if(responseType == 'text') {
+                callback(xhr.responseText);
+            } else {
+                callback(xhr.response);
+            }
+        } else {
+            callback(null);
+        }
+    };
+    xhr.send();
+};
